e8ae5ed514af3a315b497e077564250db9ad5474
milanakdj <milanakdj@gmail.com>
2026-06-05 11:02:20 +0000

    new repo

--- training.py
+++ training.py
@@ -55,7 +55,7 @@ login(token=hf_token)
 # 2. Config
 # =============================================================================
 
-OUTPUT_REPO   = "milanakdj/indic-parler-tts-nepali-finetuned-dgx-v11-cosine"
+OUTPUT_REPO   = "milanakdj/indic-parler-tts-nepali-finetuned-dgx-v12-cosine"
 DATASET_REPO  = "Titung/nepali-tts-tagged-combined"
 FINETUNE_BASE = "ai4bharat/indic-parler-tts-pretrained"
 
